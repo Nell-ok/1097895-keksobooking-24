e9f860a1d244a11e6c4abce058c9fc221dc45ed0
Nell-ok <nelli-nn@yandex.ru>
2021-10-15 12:30:30 +0300

    Фотографии

--- js/popup.js
+++ js/popup.js
@@ -2,7 +2,7 @@ import {createOffers} from './data.js';
 import {mapCanvasElement} from './map.js';
 
 const templateCard = document.querySelector('#card').content;
-const popup = templateCard.querySelector('.popup');
+const popupElement = templateCard.querySelector('.popup');
 
 const CAPTIONS = {
   palace: 'Дворец для palace',
@@ -14,11 +14,28 @@ const CAPTIONS = {
 
 const similarOffers = createOffers();
 
-function createPopup (newItem) {
-  const newPopup = popup.cloneNode(true);
+const createPopup = (newItem) => {
+  const newPopup = popupElement.cloneNode(true);
   const popupFeatures = newPopup.querySelector('.popup__features');
   const featureList = popupFeatures.querySelectorAll('.popup__feature');
   const newFeatures = newItem.offer.features;
+  const popupPhotos = newPopup.querySelector('.popup__photos');
+  const photoElement = popupPhotos.querySelector('.popup__photo');
+  const newPhotosArray = newItem.offer.photos;
+  const popupFragment = document.createDocumentFragment();
+
+  for(let i = 0; i < newPhotosArray.length; i++) {
+    const newPhotoElement = document.createElement('img');
+    newPhotoElement.classList.add('popup__photo');
+    newPhotoElement.src = newPhotosArray[i];
+    newPhotoElement.alt = 'Фотография жилья';
+    newPhotoElement.width = 45;
+    newPhotoElement.height = 40;
+    popupFragment.appendChild(newPhotoElement);
+  }
+
+  popupPhotos.appendChild(popupFragment);
+  photoElement.remove();
 
   const getCaption = (name) => CAPTIONS[name];
 
@@ -36,10 +53,9 @@ function createPopup (newItem) {
   newPopup.querySelector('.popup__text--capacity').textContent = `${newItem.offer.rooms} комнаты для ${newItem.offer.guests} гостей`;
   newPopup.querySelector('.popup__text--time').textContent = `Заезд после ${newItem.offer.checkin}, выезд до ${newItem.offer.checkout}`;
   newPopup.querySelector('.popup__description').textContent = newItem.offer.description;
-  newPopup.querySelector('.popup__photo').src = newItem.offer.photos;
   newPopup.querySelector('.popup__avatar').src = newItem.author;
 
   mapCanvasElement.appendChild(newPopup);
-}
+};
 
 createPopup(similarOffers[0]);
