7a339494396a09944dcfbf832d5030d777eedd6c
Nell-ok <nelli-nn@yandex.ru>
2021-10-19 21:06:05 +0300

    Добавление  валидации

--- js/form.js
+++ js/form.js
@@ -1,3 +1,9 @@
+const MIN_LENGTH_TITLE = 30;
+const MAX_LENGTH_TITLE = 100;
+const MAX_PRICE = 1000000;
+const inputTitle = document.querySelector('[name="title"]');
+const inputPrice = document.querySelector('[name="price"]');
+
 const adForm = document.querySelector('.ad-form');
 const adFormElements = adForm.querySelectorAll('.ad-form__element');
 const adFormHeaderElement = adForm.querySelector('.ad-form-header');
@@ -45,3 +51,70 @@ const setFormActive = () => {
 
 setFormDisabled();
 setFormActive();
+
+inputTitle.addEventListener('input', () => {
+  const valueLength = inputTitle.value.length;
+  if (valueLength < MIN_LENGTH_TITLE) {
+    inputTitle.setCustomValidity(`Еще ${MIN_LENGTH_TITLE - valueLength} симв.`);
+  } else if (valueLength > MAX_LENGTH_TITLE) {
+    inputTitle.setCustomValidity(`Удалите лишние ${valueLength - MAX_LENGTH_TITLE} симв.`);
+  } else {
+    inputTitle.setCustomValidity('');
+  }
+  inputTitle.reportValidity();
+});
+
+inputPrice.addEventListener('input', () => {
+  const valuePrice = inputPrice.value;
+  if (valuePrice > MAX_PRICE) {
+    inputPrice.setCustomValidity(`Максимальное значение ${MAX_PRICE}`);
+  } else {
+    inputPrice.setCustomValidity('');
+  }
+  inputPrice.reportValidity();
+});
+
+const selectRooms = document.querySelector('[name="rooms"]');
+const selectCapacity = document.querySelector('[name="capacity"]');
+
+selectRooms.addEventListener('change', () => {
+  if(selectRooms.options[0].selected) {
+    selectCapacity.options[0].setAttribute('disabled', 'disabled');
+    selectCapacity.options[1].setAttribute('disabled', 'disabled');
+    selectCapacity.options[3].setAttribute('disabled', 'disabled');
+    selectCapacity.options[2].removeAttribute('disabled', 'disabled');
+  } else if(selectRooms.options[1].selected) {
+    selectCapacity.options[0].setAttribute('disabled', 'disabled');
+    selectCapacity.options[1].removeAttribute('disabled', 'disabled');
+    selectCapacity.options[3].setAttribute('disabled', 'disabled');
+    selectCapacity.options[2].removeAttribute('disabled', 'disabled');
+  } else if(selectRooms.options[2].selected) {
+    selectCapacity.options[0].removeAttribute('disabled', 'disabled');
+    selectCapacity.options[1].removeAttribute('disabled', 'disabled');
+    selectCapacity.options[3].setAttribute('disabled', 'disabled');
+    selectCapacity.options[2].removeAttribute('disabled', 'disabled');
+  } else if(selectRooms.options[3].selected) {
+    selectCapacity.options[3].removeAttribute('disabled', 'disabled');
+    selectCapacity.options[0].setAttribute('disabled', 'disabled');
+    selectCapacity.options[2].setAttribute('disabled', 'disabled');
+    selectCapacity.options[1].setAttribute('disabled', 'disabled');
+  }
+});
+
+/*const createDependency = (roomElement, capacityElement) => {
+  const valueRoom = +roomElement.value;
+  for (let j = 0; j < capacityElement.options.length; j++) {
+    const valueCapacity = +capacityElement.options[j].value;
+    if (valueRoom !== 0 && valueCapacity !== 100 && valueRoom > valueCapacity) {
+      roomElement.setCustomValidity('Количество комнат не соответствует количеству гостей');
+    } else if (valueRoom !== 0 && valueCapacity === 100) {
+      roomElement.setCustomValidity('Не для гостей');
+    } else {
+      roomElement.setCustomValidity('');
+    }
+  }
+  roomElement.reportValidity();
+};
+
+selectRooms.addEventListener('change', createDependency(selectRooms, selectCapacity));
+selectCapacity.addEventListener('change', createDependency(selectRooms, selectCapacity));*/
